c4bc71e976d587e4aba3d995770702921fa934d0
anna-vaughn <60945664+anna-vaughn@users.noreply.github.com>
2026-01-17 14:55:53 -0600

    Submission Attempt 4

--- assignment1.py
+++ assignment1.py
@@ -12,7 +12,9 @@ data_p.columns = ['ds', 'y'] # Renaming the columns per Prophet's requirements.
 model = Prophet()
 modelFit = model.fit(data_p)
 
-# Create an empty dataframe with dates for future periods
-future = modelFit.make_future_dataframe(periods=744, freq='h')
-# Fill in dataframe wtih forecasts of `y` for the future periods
-pred = modelFit.predict(future)
+# Get future data to make predictions.
+data_test = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
+data_f = data_test[['Timestamp']]
+data_f.columns = ['ds']
+
+pred = modelFit.predict(data_f)
