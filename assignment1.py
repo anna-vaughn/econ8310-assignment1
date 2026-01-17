1fcefa9eb217125cff71856b4bc73d1c15c03c0f
anna-vaughn <60945664+anna-vaughn@users.noreply.github.com>
2026-01-17 16:23:35 -0600

    Submission attempt 6
    Trying new model

--- assignment1.py
+++ assignment1.py
@@ -1,21 +1,21 @@
+from pygam import LinearGAM, s, f
 import pandas as pd
-from prophet import Prophet
 
 # Get data.
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 data['Timestamp'] = pd.to_datetime(data['Timestamp'])
 
-# Get only timestamp and time series data from the dataframe.
-data_p = data[['Timestamp', 'trips']]
-data_p.columns = ['ds', 'y'] # Renaming the columns per Prophet's requirements.
+# Grab timestamp columns and data.
+x = data[['year', 'month', 'day', 'hour']]
+y = data['trips']
 
-model = Prophet()
-modelFit = model.fit(data_p)
+# Create the model.
+model = LinearGAM(f(0) + f(1) + s(2) + s(3))
+# Fit the model.
+modelFit = model.fit(x, y)
 
-# Get future data to make predictions.
-data_test = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
-data_f = data_test[['Timestamp']]
-data_f.columns = ['ds']
-
-pred = modelFit.predict(data_f)
-pred = pred[['trend']]
+# Make predictions using the fitted model.
+pred = modelFit.predict(x)
+pred = pd.DataFrame({'trend': pred}) # Convert to DF.
+pred = pred.tail(744)
+pred = pred.reset_index(drop=True)
